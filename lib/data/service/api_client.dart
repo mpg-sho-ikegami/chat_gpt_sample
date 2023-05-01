032b4fc1212d9728a2589a1eb9a0b79e9fb6599d
mpg-sho-ikegami <sho.ikegami@medpeer.co.jp>
2023-05-01 17:03:18 +0900

    API ClientのProvider追加

--- lib/data/service/api_client.dart
+++ lib/data/service/api_client.dart
@@ -1,9 +1,19 @@
 import 'package:chat_gpt_sample/data/create_completion_request.dart';
 import 'package:chat_gpt_sample/data/create_completion_response.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:retrofit/retrofit.dart';
 
 part 'api_client.g.dart';
+
+final dioProvider = Provider<Dio>((_) {
+  return Dio();
+});
+
+final apiClientProvider = Provider<ApiClient>((ref) {
+  final _dio = ref.watch(dioProvider);
+  return ApiClient(_dio);
+});
 
 @RestApi(baseUrl: "https://api.openai.com/v1")
 abstract class ApiClient {
